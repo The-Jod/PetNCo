05dcc0f6968c4f7f4827b821ec9226696675f035
The-Jod <tjdrxd@gmail.com>
2024-10-22 22:15:45 -0300

    Actualizacion de HOSTS

--- Petnco/settings.py
+++ Petnco/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
-ALLOWED_HOSTS = ['petnco.onrender.com',]
+ALLOWED_HOSTS = ['petnco.onrender.com', 'localhost',]
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
