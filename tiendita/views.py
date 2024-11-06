2ba441d92b8efab316eec6e9702741e594018f78
CmKllr <camilo.rodguez@gmail.com>
2024-11-06 00:21:02 -0300

    Cambios vista "Citas ", Integración vista Login

--- tiendita/views.py
+++ tiendita/views.py
@@ -11,7 +11,7 @@ def reg_view(request):
     return render(request, 'lateregistration/registro.html')
 
 def login_view(request):
-    return render(request, 'lateregistration/login.html')
+    return render(request, 'login.html')
 
 def home_view(request):
     return render(request, 'home.html')
